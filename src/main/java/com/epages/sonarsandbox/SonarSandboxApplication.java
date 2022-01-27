bf818c97b2b628bd1ceb2694973ef8ac846a8412
Jan Mewes <5235584+jmewes@users.noreply.github.com>
2022-01-27 15:58:12 +0100

    Initial commit

---
+++ src/main/java/com/epages/sonarsandbox/SonarSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.epages.sonarsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epages/sonarsandbox/SonarSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epages.sonarsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
