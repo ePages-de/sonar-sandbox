6a0205c41c9ff2ab758517800b799e3122f35b07
Roberto Filho <r.welzel@epages.com>
2022-01-27 17:30:27 +0100

    Adding more bad code

--- src/main/java/com/epages/sonarsandbox/HelloService.java
+++ src/main/java/com/epages/sonarsandbox/HelloService.java
@@ -1,8 +1,25 @@
 package com.epages.sonarsandbox;
 
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.springframework.util.StringUtils;
+
 public class HelloService {
 
     public String sayHelloWithNoCoverage() {
+        return Greetings.createMessage();
+    }
+
+    public String sayHelloDuplicate() {
+        return Greetings.createMessage();
+    }
+
+    public String sayHello1() {
+        return Greetings.createMessage();
+    }
+
+    public String sayHello2() {
         return Greetings.createMessage();
     }
 }
